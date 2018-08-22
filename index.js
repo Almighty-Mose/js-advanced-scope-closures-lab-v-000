2ebd5cc8d4f52fb1ba9a3fc617cfc87166bcc9a8
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-22 23:44:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function produceDrivingRange(range) {
     let beginning = parseInt(beginningLocation)
     let end = parseInt(endingLocation)
     let distance = Math.abs(beginning - end)
+    let difference = distance - range;
     if(distance <= range) {
       return ""
     }
