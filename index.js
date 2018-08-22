5b2646c2e5daaf9e58d09c52d2d61c84ec397dc3
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-22 23:50:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function produceDrivingRange(range) {
+  return function(beginningLocation, endingLocation) {
+    let beginning = parseInt(beginningLocation);
+    let end = parseInt(endingLocation);
+    let travelDistance = Math.abs(beginning - end);
+    let difference = range - travelDistance;
+    
+    if(difference > 0){
+      return `within range by ${difference}`;
+    } else {
+      return `${Math.abs(difference)} blocks out of range`;
+    }
+  };
+}
+
+function produceTipCalculator(percentage) {
+  return function(fare) {
+    return fare * percentage;
+  };
+}
+
+function createDriver() {
+  let driverId = 0;
+  
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  };
+}
